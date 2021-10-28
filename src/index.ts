8ee77f6f8e20c6348a54f8fe0dfe462c2c949539
KGESH <baram789456@gmail.com>
2021-10-28 17:17:05 +0900

    start refector class amoutfor

--- src/index.ts
+++ src/index.ts
@@ -1,88 +1,31 @@
 import Invoices from './invoices';
 import Plays from './plays';
+import { createStatementData } from './createStatementData';
 
 export const statement = (invoice: any, plays: any) => {
   return renderPlainText(createStatementData(invoice, plays));
 };
 
-export const createStatementData = (invoice: any, plays: any) => {
-  const enrichPerformance = (aPerformance: any) => {
-    const result = Object.assign({}, aPerformance);
-    result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
-    return result;
-  };
+export const htmlStatement = (invoice: any, plays: any) => {
+  return renderHTML(createStatementData(invoice, plays));
+};
 
-  const totalVolumeCredits = (data: any) => {
-    return data.performances.reduce((total: number, p: any) => total + p.volumeCredits, 0);
-  };
+export const renderHTML = (data: any) => {
+  let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+  result += `<table>\n`;
+  result += `<tr><th>연극</th><th>좌석 수</th><th>금액</></tr>`;
 
-  const totalAmount = (data: any) => {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount;
-    }
-
-    return data.performances.reduce((total: number, p: any) => total + p.amount, 0);
-  };
-  const volumeCreditsFor = (aPerformance: any) => {
-    let volumeCredits = 0;
-    volumeCredits += Math.max(aPerformance.audience - 30, 0);
-
-    if ('comedy' === aPerformance.play.type) {
-      volumeCredits += Math.floor(aPerformance.audience / 5);
-    }
-
-    return volumeCredits;
-  };
-
-  const playFor = (aPerformance: any) => {
-    return plays[aPerformance.playID];
-  };
-
-  const amountFor = (aPerformance: any) => {
-    let result = 0;
-    switch (aPerformance.play.type) {
-      case 'tragedy':
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-
-      case 'comedy':
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 30);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-    }
-
-    return result;
-  };
-
-  let statementData: any = {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-
-  return statementData;
+  for (let perf of data.performances) {
+    result += `<tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
+  }
+  result += `</table>\n`;
+  result += `<p>총액: <em> ${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+  return result;
 };
 
 export const renderPlainText = (data: any) => {
-  const usd = (aNumber: number) => {
-    return new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100);
-  };
   let result = `청구 내역 (고객명: ${data.customer})\n`;
 
   for (let perf of data.performances) {
@@ -92,4 +35,12 @@ export const renderPlainText = (data: any) => {
   result += `총액: ${usd(data.totalAmount)}\n`;
   result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
   return result;
+};
+
+const usd = (aNumber: number) => {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100);
 };
