26f55abcaf1e60da9c4f31d9b13398daf1a43cb6
KGESH <baram789456@gmail.com>
2021-10-28 10:43:46 +0900

    함수 추출하기

--- src/index.ts
+++ src/index.ts
@@ -11,33 +11,37 @@ export const statement = (invoice: any, plays: any) => {
     minimumFractionDigits: 2,
   }).format;
 
-  const amountFor = (perf: any, play: any) => {
-    let thisAmount = 0;
+  const playFor = (aPerformance: any) => {
+    return plays[aPerformance.playID];
+  };
+
+  const amountFor = (aPerformance: any, play: any) => {
+    let result = 0;
     switch (play.type) {
       case 'tragedy':
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
         }
         break;
 
       case 'comedy':
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 30);
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 30);
         }
-        thisAmount += 300 * perf.audience;
+        result += 300 * aPerformance.audience;
         break;
 
       default:
         throw new Error(`알 수 없는 장르: ${play.type}`);
     }
 
-    return thisAmount;
+    return result;
   };
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    const play = playFor(perf);
     let thisAmount = amountFor(perf, play);
 
     volumeCredits += Math.max(perf.audience - 30, 0);
