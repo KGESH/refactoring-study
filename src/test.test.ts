43b1c4e3df1d8e996c10998bf4ca70d69c4ff55a
KGESH <baram789456@gmail.com>
2021-10-28 10:38:01 +0900

    jest config done

--- src/test.test.ts
+++ src/test.test.ts
@@ -1,17 +1,19 @@
-// import Invoices from './invoices';
-//import Plays from './plays';
-// import { statement } from './index';
-const Invoices = require('../js/invoices.json');
-const Plays = require('../js/plays.json');
-const statement = require('./index.ts');
+import Invoices from './invoices';
+import Plays from './plays';
+import { statement } from './index';
+// const Invoices = require('../js/invoices.json');
+// const Plays = require('../js/plays.json');
+// const statement = require('./index.ts');
 
 describe('test index.ts', () => {
   it('should be match result string', () => {
-    expect(statement(Invoices, Plays)).toMatch(`청구 내역 (고객명: BigCo)
-    Hamlet: $650.00 (55석)
-    As You Like It: $530.00 (35석)
-    Othello: $500.00 (40석)
-    총액: $1,680.00
-    적립 포인트: 47점`);
+    expect(statement(Invoices, Plays).replace(/\s/g, '')).toMatch(
+      `청구 내역 (고객명: BigCo)\n
+    Hamlet: $650.00 (55석)\n
+    As You Like It: $530.00 (35석)\n
+    Othello: $500.00 (40석)\n
+    총액: $1,680.00\n
+    적립 포인트: 47점\n`.replace(/\s/g, ''),
+    );
   });
 });
